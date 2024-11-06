e0a9bf1525accc1eefd14a7de15715976820b40b
buriy40 <buriy40@bk.ru>
2024-11-06 10:48:40 +0300

    вывод таблицы в консоль

--- Lesson.py
+++ Lesson.py
@@ -14,15 +14,18 @@ from random import random
 # print_name('Oleg')
 # print_name('Sergey')
 
-import random
-def lottery(*args,**kwargs):
-    tickets = [1, 2, 3 ,4, 5 , 6 , 7 , 8, 9, 10]
-    win1 = random.choice(tickets)
-    tickets.remove(win1)
-    win2 = random.choice(tickets)
-    print(*args)
-    return win1, win2
+# import random
+# def lottery(*args,**kwargs):
+#     tickets = [1, 2, 3 ,4, 5 , 6 , 7 , 8, 9, 10]
+#     win1 = random.choice(tickets)
+#     tickets.remove(win1)
+#     win2 = random.choice(tickets)
+#     print(*args)
+#     return win1, win2
+#
+# win1, win2 = lottery('mon', 'tue')
+# print(win1, win2)
 
-win1, win2 = lottery('mon', 'tue')
-print(win1, win2)
+
+def test(a =2)
 
